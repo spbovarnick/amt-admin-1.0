649519b4cf7237cd439ad2aa5a0dd3e6dcf78723
spbovarnick <spbovarnick@gmail.com>
2024-01-18 10:22:28 -0800

    archive_item_form.js pinned in importmap.rb  and imported singly to views/archive_items/_form, import removed from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./src/archive_items_form";
+// import "./src/archive_items_form";
 import "./src/user_registration_form";
 import "./src/direct_uploads";
 import "./src/news_items_form"
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/src", under: "src", to: "src"
 pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.8/dist/trix.esm.min.js", preload: true
 pin "@rails/actiontext", to: "actiontext.js"
-# subsequent modules are pinned here to be used in specific templates
+# subsequent modules are pinned here to be used in specific form partials where they're used/needed
 pin "admin", preload: true
+pin "archive_items_form", preload: true

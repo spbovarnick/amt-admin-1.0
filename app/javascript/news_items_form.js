1eacc8be545e2e5df7284f34306818a0bd571011
spbovarnick <spbovarnick@gmail.com>
2024-01-18 10:36:04 -0800

    some optional chaining added to news_items_form.js to prevent browser console errors

--- app/javascript/news_items_form.js
+++ app/javascript/news_items_form.js
@@ -5,17 +5,17 @@ document.addEventListener("turbo:load", function () {
 function listenForClear() {
   const clearButton = document.querySelector(".clear_attachement_button");
   const uploadInput = document.querySelector(
-    `#${clearButton.dataset.baseInputId}`
+    `#${clearButton?.dataset.baseInputId}`
   );
-  const input = document.querySelector(`#${clearButton.dataset.input}`);
-  const preview = document.querySelector(`#${clearButton.dataset.preview}`);
+  const input = document.querySelector(`#${clearButton?.dataset.input}`);
+  const preview = document.querySelector(`#${clearButton?.dataset.preview}`);
 
-  uploadInput.addEventListener("change", function () {
+  uploadInput?.addEventListener("change", function () {
     // prevent attachement from being purged if new item is uploaded
     input.value = "false";
   });
 
-  clearButton.addEventListener("click", function () {
+  clearButton?.addEventListener("click", function () {
     // pass "true" to controller, so it knows to purge the attachement
     input.value = "true";
     // hide the preview

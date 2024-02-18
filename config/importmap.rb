8249a6dd37c81ecc25aeac5a8ddc2c5825980970
spbovarnick <spbovarnick@gmail.com>
2024-02-17 18:01:01 -0800

    changed preload to false for local modules to fix MIME type error in prod

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,8 +9,8 @@ pin_all_from "app/javascript/src", under: "src", to: "src"
 pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.8/dist/trix.esm.min.js", preload: true
 pin "@rails/actiontext", to: "actiontext.js"
 # subsequent modules are pinned here to be used in specific form partials where they're used/needed
-pin "admin", preload: true
-pin "archive_items_form", preload: true
-pin "user_registration_form", preload: true
-pin "news_items_form", preload: true
-pin "draft_select", preload: true
+pin "admin", preload: false
+pin "archive_items_form", preload: false
+pin "user_registration_form", preload: false
+pin "news_items_form", preload: false
+pin "draft_select", preload: false

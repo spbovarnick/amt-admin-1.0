810923725eba171c825ea1a77d029a6adcee4bd9
spbovarnick <spbovarnick@gmail.com>
2024-02-17 19:06:38 -0800

    js all imported to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "archive_items_form";
-import "user_registration_form";
-import "src/direct_uploads";
-import "news_items_form"
 import "trix";
 import "@rails/actiontext";
+import "src/direct_uploads";
+import "archive_items_form";
+import "user_registration_form";
+import "admin";
+import "draft_select";

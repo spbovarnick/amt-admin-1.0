9105e5b2e983a2f7aa898539c90b96464e9fcc06
spbovarnick <spbovarnick@gmail.com>
2024-02-17 18:30:55 -0800

    importing local modules to javascript/application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-// import "./src/archive_items_form";
-// import "./src/user_registration_form";
-import "./src/direct_uploads";
-// import "./src/news_items_form"
+import "archive_items_form";
+import "user_registration_form";
+import "src/direct_uploads";
+import "news_items_form"
 import "trix";
 import "@rails/actiontext";

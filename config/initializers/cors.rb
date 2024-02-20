4c79337bca8acaea34dc3b2041112075559f9c23
spbovarnick <spbovarnick@gmail.com>
2024-02-19 17:52:56 -0800

    all resources allowed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3333', 'localhost:3000', 'https://amt-front-end.vercel.app'
-    resource 'api/v1/*', headers: :any, 
+    resource '*', 
+    headers: :any, 
     methods: [:get]
   end
 end

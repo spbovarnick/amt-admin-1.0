fe1c4bafefa3b1672246b11867edcbdd4f57caab
spbovarnick <spbovarnick@gmail.com>
2024-02-19 16:31:56 -0800

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3333', 'localhost:3000', '127.0.0.1:3000'
-    resource '*', headers: :any, 
+    origins 'localhost:3333', 'localhost:3000', '127.0.0.1:3000', 'amt-front-end.vercel.app'
+    resource 'api/v1/*', headers: :any, 
     methods: [:get]
   end
 end

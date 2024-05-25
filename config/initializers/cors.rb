3efd993f7f464bf1ed5998d82b1e73e4ee6ad396
spbovarnick <spbovarnick@gmail.com>
2024-05-25 14:41:24 -0700

    cors updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,9 +2,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3333', 'localhost:3000', 'https://amt-front-end.vercel.app', "https://albinacommunityarchive.org", "https://www.albinacommunityarchive.org"
-    resource '*', 
-    headers: :any, 
-    methods: [:get]
+    origins 'http://localhost:3333', 'http://localhost:3000', 'https://amt-front-end.vercel.app', "https://albinacommunityarchive.org", "https://www.albinacommunityarchive.org"
+    resource '*',
+    headers: :any,
+    methods: [:get, :post, :patch, :put]
   end
 end

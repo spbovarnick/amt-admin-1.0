4952bb114f39de785e633e5a7f59d35706e51974
spbovarnick <spbovarnick@gmail.com>
2025-04-09 13:04:33 -0700

    patch edit

--- config/initializers/force_s3_acl.rb
+++ config/initializers/force_s3_acl.rb
@@ -1,25 +1,17 @@
 # config/initializers/force_s3_acl.rb
 require "active_storage/service/s3_service"
 
-Rails.application.config.to_prepare do
-  ActiveStorage::Service::S3Service.class_eval do
-    private
-
-    # This helper returns the S3 object for a given key.
-    def object_for(key)
-      bucket.object(key)
-    end
-
-    # Override the upload method to force the ACL on every upload.
-    def upload(key, io, checksum: nil, **options)
-      instrument :upload, key: key, checksum: checksum do
-        object_for(key).upload_stream(
-          body: io,
-          content_md5: checksum,
-          acl: "bucket-owner-full-control", # Force the ACL here.
-          **options
-        )
-      end
+module ForceS3Acl
+  def upload(key, io, checksum: nil, **options)
+    instrument :upload, key: key, checksum: checksum do
+      bucket.object(key).upload_stream(
+        body: io,
+        content_md5: checksum,
+        acl: "bucket-owner-full-control",  # Force this ACL on every upload
+        **options
+      )
     end
   end
 end
+
+ActiveStorage::Service::S3Service.prepend(ForceS3Acl)

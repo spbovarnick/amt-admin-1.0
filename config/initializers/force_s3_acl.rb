2e19a3bb3fb8a2d24b0234d973bd74af7a7c4577
spbovarnick <spbovarnick@gmail.com>
2025-04-09 13:12:10 -0700

    patch attempt 3

--- config/initializers/force_s3_acl.rb
+++ config/initializers/force_s3_acl.rb
@@ -1,16 +1,9 @@
-# config/initializers/force_s3_acl.rb
 require "active_storage/service/s3_service"
 
 module ForceS3Acl
   def upload(key, io, checksum: nil, **options)
-    instrument :upload, key: key, checksum: checksum do
-      bucket.object(key).upload_stream(
-        body: io,
-        content_md5: checksum,
-        acl: "bucket-owner-full-control",  # Force this ACL on every upload
-        **options
-      )
-    end
+    # Merge in our desired ACL so that every upload gets "bucket-owner-full-control"
+    super(key, io, checksum: checksum, **options.merge(acl: "bucket-owner-full-control"))
   end
 end
 

a8c868c3c770ffa86ee0e07fdda9cf6a412e71f3
spbovarnick <spbovarnick@gmail.com>
2025-05-22 13:26:46 -0700

    sending presigned urls through email successfully, caught typo in csv_mailer erb

--- app/jobs/export_archive_items_csv_job.rb
+++ app/jobs/export_archive_items_csv_job.rb
@@ -5,12 +5,15 @@ class ExportArchiveItemsCsvJob < ApplicationJob
     user = User.find(user_id)
     file = Tempfile.new(["archive_items", ".csv"])
 
+    # select columns and headers to use
     attributes = %w{ uid search_collections title created_by created_at medium credit year search_comm_groups search_people search_tags }
     headers = [ "UID",  "Collection",  "Title",  "Created By",  "Created At",  "Medium",  "Credit",  "Year",  "Community Groups",  "People",  "Tags", "Content Notes", "Medium Technical Notes", "Content Files" ]
 
+    # write csv
     CSV.open(file.path, "w") do |csv|
       csv << headers
 
+      # pull content file urls
       ArchiveItem.includes( {content_files_attachments: :blob }, :rich_text_content_notes, :rich_text_medium_notes).find_each(batch_size: 100) do |item|
         urls = item.content_files.map do |file|
           Rails.application.routes.url_helpers.url_for(file)
@@ -18,6 +21,7 @@ class ExportArchiveItemsCsvJob < ApplicationJob
             "Error: #{e.message}"
         end.join(", ")
 
+        # transform block text to plain
         content_notes = item.content_notes&.to_plain_text || ""
         medium_notes = item.medium_notes&.to_plain_text || ""
 
@@ -27,7 +31,10 @@ class ExportArchiveItemsCsvJob < ApplicationJob
       end
     end
 
-    s3_key = "exports/archive_items_#{Time.now.to_i}.csv"
+    # legible/organizational string name
+    file_id_string = [Time.now.year, Time.now.mon, Time.now.mday].join('-') + '_' + [ Time.now.hour, Time.now.min, Time.now.sec].join(':')
+
+    s3_key = "exports/archive_items_#{file_id_string}.csv"
     s3_url = upload_to_s3(file.path, s3_key)
 
     CsvMailer.with(user:, url: s3_url).csv_ready.deliver_later
@@ -41,11 +48,27 @@ class ExportArchiveItemsCsvJob < ApplicationJob
 
   def upload_to_s3(path, key)
     s3 = Aws::S3::Resource.new(region: 'us-west-2')
-    bucket = s3.bucket(ENV.fetch("S3_BUCKET_NAME"))
+    bucket = s3.bucket(ENV["S3_BUCKET_NAME"])
 
     obj = bucket.object(key)
     obj.upload_file(path, acl: "private")
 
-    obj.presigned_url(:get, expires_in: 604800)
+    # this bucket is private, required getting presigned url via client
+    client = Aws::S3::Client.new(
+      region: ENV['AWS_REGION'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
+
+    presigner = Aws::S3::Presigner.new(client: client)
+
+    s3_url = presigner.presigned_url(
+      :get_object,
+      bucket: ENV['S3_BUCKET_NAME'],
+      key: obj.key,
+      expires_in: 604800
+    )
+
+    return s3_url
   end
 end
--- app/mailers/CsvMailer.rb
+++ app/mailers/CsvMailer.rb
@@ -2,6 +2,7 @@ class CsvMailer < ApplicationMailer
   def csv_ready
     @user = params[:user]
     @url = params[:url]
+    puts "From Mailer: #{@url}"
     mail(to: @user.email, subject: "Your Archive Items CSV is Ready")
   end
 end

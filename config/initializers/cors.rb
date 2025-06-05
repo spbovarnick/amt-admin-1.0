780cdaed168edc5c86f80b8af9fee32c7c931f61
spbovarnick <spbovarnick@gmail.com>
2025-06-05 12:39:40 -0700

    cors for front-end staging

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3333', 'http://localhost:3000', 'https://amt-front-end.vercel.app', "https://albinacommunityarchive.org", "https://www.albinacommunityarchive.org"
+    origins 'http://localhost:3333', 'http://localhost:3000', 'https://amt-front-end.vercel.app', "https://albinacommunityarchive.org", "https://www.albinacommunityarchive.org", "https://amt-front-end-staging.vercel.app"
     resource '*',
     headers: :any,
     methods: [:get, :post, :patch, :put]

bb7b662b7bb007ae59c184b777a1eba68156117e
spbovarnick <spbovarnick@gmail.com>
2025-08-14 11:09:18 -0700

    admin UI working

--- app/javascript/controllers/featured_item_controller.js
+++ app/javascript/controllers/featured_item_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   connect() {
     const featuredItemInput = document.querySelector(".featured-item-select")
     featuredItemSelectUI(featuredItemInput)
-    console.log('fuck')
 
     function featuredItemSelectUI(input) {
       input.style.backgroundColor = input.value === "true" ? "#e8dc5a" : "#313131";
       input.addEventListener("change", function () {
         input.style.backgroundColor = input.value === "true" ? "#e8dc5a" : "#313131";
+        input.style.color = input.value === "true" ? "black" : "white";
       })
     }
   }

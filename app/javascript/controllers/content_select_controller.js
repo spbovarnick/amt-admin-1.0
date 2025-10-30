0e72170b391d2f50739f127673e7fb31189fba5f
spbovarnick <spbovarnick@gmail.com>
2025-10-30 13:04:51 -0700

    poster image toggles through content select toggle

--- app/javascript/controllers/content_select_controller.js
+++ app/javascript/controllers/content_select_controller.js
@@ -14,17 +14,30 @@ export default class extends Controller {
     input.addEventListener("change", function () {
 
       if (input.value === "true") {
+        const posterRow = document.querySelector("#poster-image-row");
+
         linksList.style.display = "block";
         linksList.disabled = false;
 
         filesList.style.display = "none";
         filesList.disabled = true;
+
+        if (input.value !== "film" || input.value !== "audio" || input.value !== "article" || input.value !== "printed material" && posterRow){
+          posterRow.classList.remove("hidden");
+        }
+
       } else {
+        const posterRow = document.querySelector("#poster-image-row");
+
         linksList.style.display = "none";
         linksList.disabled = true;
 
         filesList.style.display = "block";
         filesList.disabled = false;
+
+        if (input.value !== "film" || input.value !== "audio" || input.value !== "article" || input.value !== "printed material" && posterRow) {
+          posterRow.classList.add("hidden");
+        }
       }
     })
   }

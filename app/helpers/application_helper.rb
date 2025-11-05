4a7c5ae91ca3030dc33461a48d222a89dd2666ed
spbovarnick <spbovarnick@gmail.com>
2025-11-05 12:29:56 -0800

    styled, UI updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,13 @@ module ApplicationHelper
       user.persisted? ? user_registration_path(user) : registration_path(:admin_user)
   end
 
-  # updates method used in user registration form_with for new or edit 
+  # updates method used in user registration form_with for new or edit
   def user_form_method(user)
       user.persisted? ? :put : :post
+  end
+
+  def render_svg(icon, classes: nil)
+    classes ||= ""
+    render "icons/#{icon}", classes: classes
   end
 end

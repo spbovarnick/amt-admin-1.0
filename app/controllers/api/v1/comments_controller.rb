fb2071c8acab7d443d635f91187152bed6529d72
spbovarnick <spbovarnick@gmail.com>
2026-01-24 20:13:08 -0800

    mailer updated to be multi-functional

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CommentsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        ArchiveCommentMailer.comment_email(params[:title], params[:id], params[:uid], params[:first_name], params[:last_name], params[:email_addy], params[:subject], params[:comment]).deliver_later
+        ArchiveCommentMailer.comment_email(params[:item_comment], params[:title], params[:id], params[:uid], params[:first_name], params[:last_name], params[:email_addy], params[:subject], params[:comment]).deliver_later
         render json: { status: 'success', message: "Email sent!"}
     rescue => e
         Rails.logger.error(e)
--- app/mailers/archive_comment_mailer.rb
+++ app/mailers/archive_comment_mailer.rb
@@ -1,17 +1,21 @@
 class ArchiveCommentMailer < ApplicationMailer
   default from: -> { Rails.application.credentials[Rails.env.to_sym][:gmail] }
 
-    def comment_email(title, id, uid, first_name, last_name, email_addy, subject, comment)
-        @title = title
-        @id = id
-        @uid = uid
+    def comment_email(item_comment, title, id, uid, first_name, last_name, email_addy, subject, comment)
+        @item_comment = item_comment
         @first_name = first_name
         @last_name = last_name
         @email_addy = email_addy
         @subject = subject
         @comment = comment
+        if item_comment
+            @title = title
+            @id = id
+            @uid = uid
+            @subject = subject + " " + uid
+        end
         mail(to: Rails.application.credentials[Rails.env.to_sym][:gmail],
-            subject: subject + " " + @uid,
+            subject: @subject,
         )
     end
 end

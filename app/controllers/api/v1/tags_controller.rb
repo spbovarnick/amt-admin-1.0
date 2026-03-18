ff3a85785b4a64590582fd5975e7a84abc065f42
spbovarnick <spbovarnick@gmail.com>
2026-03-18 10:47:57 -0700

    logging tags, test in staging

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::TagsController < ApplicationController
   include ActiveStorage::SetCurrent
   def index
     tags = ArchiveTag.all.order(name: :asc)
+    Rails.logger.info "All tags # " + tags
     render json: tags
   end
 end

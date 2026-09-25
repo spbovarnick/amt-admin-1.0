344b083334955ea80ab85d40aa0355e15e3eae1c
spbovarnick <spbovarnick@gmail.com>
2026-09-25 09:33:04 -0700

    update endpoint to only count published items

--- app/controllers/api/v1/archive_items_controller.rb
+++ app/controllers/api/v1/archive_items_controller.rb
@@ -80,8 +80,8 @@ class Api::V1::ArchiveItemsController < ApplicationController
   end
 
   def tally_counts
-    files_count = ActiveStorage::Attachment.where(record_type: "ArchiveItem", name: "content_files").count
-    items_count = ArchiveItem.all.count
+    files_count = ActiveStorage::Attachment.where(record_type: "ArchiveItem", name: "content_files", record_id: base_scope.select(:id)).count
+    items_count = base_scope.count
 
     render json: {files_count:, items_count:}
   end
